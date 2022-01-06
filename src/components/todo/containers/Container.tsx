3ca5adf5d1b443e6e3913362e4e7db3e0c98a2cc
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2022-01-06 15:12:32 +0900

    todo List Componentを分離

--- src/components/todo/containers/Container.tsx
+++ src/components/todo/containers/Container.tsx
@@ -1,10 +1,10 @@
 import { VFC, useState, useCallback, FormEvent } from "react";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import { Task, TaskId } from "../models/todo";
+import { Task } from "../models/todo";
 import { InputArea } from "../presenters/InputArea";
-import { Item } from "../presenters/Item";
 import { todoState } from "../store/todoState";
+import { List } from "./List";
 
 export const Container: VFC = () => {
   console.log("Container Component");
@@ -42,50 +42,15 @@ export const Container: VFC = () => {
     [id, todo]
   );
 
-  const onRemove = useCallback(
-    (id: TaskId) => {
-      setTodo(todo.filter((task) => task.id !== id));
-    },
-    [todo]
-  );
-
-  const toggleCheckBox = useCallback(
-    (id: TaskId) => {
-      const tasks = todo.map((task) => {
-        if (task.id === id) {
-          return { ...task, isDone: !task.isDone };
-        }
-        return task;
-      });
-
-      setTodo(tasks);
-    },
-    [todo]
-  );
-
   return (
     <>
       <InputArea handleSubmit={handleSubmit} />
       <Hr />
-      <List>
-        {todo.map((task) => (
-          <Item
-            key={task.id}
-            task={task}
-            toggleCheckBox={toggleCheckBox}
-            onRemove={onRemove}
-          />
-        ))}
-      </List>
+      <List />
     </>
   );
 };
 
 const Hr = styled.hr`
   width: 100%;
-`;
-
-const List = styled.ul`
-  padding: 0;
-  margin: 0;
 `;
--- src/components/todo/containers/List.tsx
+++ src/components/todo/containers/List.tsx
@@ -1,5 +1,49 @@
-import { VFC } from "react";
+import { useCallback, VFC } from "react";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { TaskId } from "../models/todo";
+import { Item } from "../presenters/Item";
+import { todoState } from "../store/todoState";
 
 export const List: VFC = () => {
-  return <li></li>;
+  const [todo, setTodo] = useRecoilState(todoState);
+
+  const onRemove = useCallback(
+    (id: TaskId) => {
+      setTodo(todo.filter((task) => task.id !== id));
+    },
+    [todo]
+  );
+
+  const toggleCheckBox = useCallback(
+    (id: TaskId) => {
+      const tasks = todo.map((task) => {
+        if (task.id === id) {
+          return { ...task, isDone: !task.isDone };
+        }
+        return task;
+      });
+
+      setTodo(tasks);
+    },
+    [todo]
+  );
+
+  return (
+    <Ul>
+      {todo.map((task) => (
+        <Item
+          key={task.id}
+          task={task}
+          toggleCheckBox={toggleCheckBox}
+          onRemove={onRemove}
+        />
+      ))}
+    </Ul>
+  );
 };
+
+const Ul = styled.ul`
+  padding: 0;
+  margin: 0;
+`;
