16f5043064c519bfc33faed5e33ba182cfefc8ed
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2022-01-06 13:44:17 +0900

    todo Page Component と Container Component に分離
    このままだと状態の管理上一つのContainerにするしかない状況

---
+++ src/components/todo/containers/Container.tsx
@@ -0,0 +1,89 @@
+import { VFC, useState, useCallback, FormEvent } from "react";
+import styled from "styled-components";
+import { Task, TaskId } from "../models/todo";
+import { InputArea } from "../presenters/InputArea";
+import { Item } from "../presenters/Item";
+
+export const Container: VFC = () => {
+  console.log("Container Component");
+
+  const [id, setId] = useState(0);
+  const [todo, setTodo] = useState<Task[]>([]);
+
+  const handleSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      // え、これでいいの!?
+      const value = (
+        e.target as typeof e.target & {
+          task: { value: string };
+        }
+      ).task.value;
+
+      const nextId = id + 1;
+      setId(nextId);
+
+      const task: Task = {
+        id: nextId,
+        isDone: false,
+        text: value,
+      };
+
+      setTodo([...todo, task]);
+
+      (
+        e.target as typeof e.target & {
+          task: { value: string };
+        }
+      ).task.value = "";
+    },
+    [id, todo]
+  );
+
+  const onRemove = useCallback(
+    (id: TaskId) => {
+      setTodo(todo.filter((task) => task.id !== id));
+    },
+    [todo]
+  );
+
+  const toggleCheckBox = useCallback(
+    (id: TaskId) => {
+      const tasks = todo.map((task) => {
+        if (task.id === id) {
+          return { ...task, isDone: !task.isDone };
+        }
+        return task;
+      });
+
+      setTodo(tasks);
+    },
+    [todo]
+  );
+
+  return (
+    <>
+      <InputArea handleSubmit={handleSubmit} />
+      <Hr />
+      <List>
+        {todo.map((task) => (
+          <Item
+            key={task.id}
+            task={task}
+            toggleCheckBox={toggleCheckBox}
+            onRemove={onRemove}
+          />
+        ))}
+      </List>
+    </>
+  );
+};
+
+const Hr = styled.hr`
+  width: 100%;
+`;
+
+const List = styled.ul`
+  padding: 0;
+  margin: 0;
+`;
